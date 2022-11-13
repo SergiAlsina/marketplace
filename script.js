5cd19d49faec99f74cf1111a3e6f4b72b472fe91
Freddy Vegas <vegasfalexis@gmail.com>
2022-11-13 22:15:23 +0100

    responsi de contacto page

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function funcion(){
     location.href = "product.html";
 }
+
+const btnMenu =document.querySelector("#btnMenu");
+const menu =document.querySelector("#menu");
+btnMenu.addEventListener("click", function(){
+   menu.classList.toggle("mostrar")  
+});
