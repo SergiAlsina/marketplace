5bf6eb9d24aa2d6daf4dbdbf2bef92e864850035
Freddy Vegas <vegasfalexis@gmail.com>
2023-01-05 14:00:35 +0100

    menu script

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-function productos(){
-    location.href = "product.html";
-}
+// function productos(){
+//     location.href = "product.html";
+// }
 
-function contactos(){
-    location.href = "contact.html";
-}
+// function contactos(){
+//     location.href = "contact.html";
+// }
 
-function carro(){
-    location.href = "cart.html";
-}
+// function carro(){
+//     location.href = "cart.html";
+// }
 
-function home(){
-    location.href = "home.html";
+// function home(){
+//     location.href = "home.html";
+// }
+
+const toggle = document.getElementById('_toggle')
+const items = document.getElementById('_items')
+
+toggle.onclick = ()=>{
+    items.classList.toggle('open')
+    items.classList.toggle('close')
 }
